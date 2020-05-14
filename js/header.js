bc725be77b95f068a987b84378baff5be50d2c70
Qiwen Hu <qiwenhu@hotmail.com>
2020-05-14 03:55:37 -0600

    100vh fix for mobile

--- js/header.js
+++ js/header.js
@@ -36,3 +36,11 @@ $(window).scroll(function() {
         $('.content').removeClass('sticky-content');
     }
 });
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
